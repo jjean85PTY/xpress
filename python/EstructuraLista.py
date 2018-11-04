e3603de8c72da6acb39e9e2e8ba65645c6781a11
jjean85PTY <jltimothee@gmail.com>
2018-11-04 15:35:57 +0800

    Pratica Lista y Funciones

---
+++ python/EstructuraLista.py
@@ -0,0 +1,20 @@
+mylist=["Jennifer", 8, "Alicia", "Kim", "Jen", True]
+#mylist2=["Dog","Cat"]
+#mylist3 = mylist+mylist2
+#mylist.append("Miranda") 
+
+#mylist.insert(2,"Miranda") #Insertar un componente mas
+
+#mylist.extend(["Mario", "Kim"])
+
+#mylist.remove() # no sale este
+#mylist.pop()
+
+#print(mylist)
+#print(mylist[1])
+#print(mylist[1:3])
+#print(mylist[:3])
+#print(mylist[2])
+#print(mylist.index("Kim"))
+#print("Kim" in mylist)
+print(mylist)
---
+++ python/funtionsuma.py
@@ -0,0 +1,19 @@
+def suma(n1,n2):
+
+	#n1=4;n2=9
+
+	#print(n1+n2)
+	resul= n1+n2
+	return resul
+
+#suma(5,7)
+
+#print("------------------------------")
+
+#suma(89,123)
+
+#print("...............................")
+
+#suma(89,123)
+almacena_resul=suma(10,9)
+print(almacena_resul)
