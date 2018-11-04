bc28f4d0ef66472fe580a59e9de99e7f45ff975e
jjean85PTY <jltimothee@gmail.com>
2018-11-04 14:28:23 +0800

    Pratica de Python

---
+++ python/firstFuntion.py
@@ -0,0 +1,13 @@
+def mensaje():
+
+	print("Este es la printica de mi primer funciones")
+	print("Este es la printica de mi primer segunda")
+	print("Este es la printica de mi primer seguimos")
+
+mensaje()
+print("-----------------------------------")
+mensaje()
+print("-----------------------------------")
+mensaje()
+print("-----------------------------------")
+
---
+++ python/ifprotices.py
@@ -0,0 +1,8 @@
+n1=1
+n2=5
+if n1>n2:
+	print("EL numero 1 es mayor")
+if n1==n2:
+	print("Ambos numero son iguels")
+else:
+	print("EL numero 2 es mayor")
